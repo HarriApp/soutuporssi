f91d692b5b125dfbc20ea4646d8341f605311b90
Harri R J Lemmelä <harri.lemmela@gmail.com>
2026-02-09 17:44:32 +0200

    Update navigation to improve UX

--- app.py
+++ app.py
@@ -7,13 +7,12 @@ import config
 import db
 import team
 
-
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    teams = team.get_teams()
+    teams = team.get_all()
     return render_template("index.html", teams=teams)
 
 @app.route("/create_team", methods=["GET", "POST"])
@@ -22,24 +21,26 @@ def new_invite():
         return render_template("create_team.html", message="")
     
     if request.method == "POST":
-        series = request.form["serie_id"]
-        team_name = request.form["team_name"]
-        description = request.form["description"]
+        if "cancel" in request.form:
+            return redirect("/")
+        
+        serie_id = request.form["serie_id"]
+        team_name = request.form["team_name"].strip()
+        description = request.form["description"].strip()
         captain = session["user_id"]
 
-        try:
-            team.create(team_name, captain, series, description)
-        except sqlite3.IntegrityError:
+        if not team.is_name_free_in_serie(team_name, serie_id):
             message = "VIRHE: Joukkueen nimi on jo varattu tässä sarjassa"
             return render_template("create_team.html", message=message)
-
+        
+        team.create(team_name, captain, serie_id, description)
         return redirect("/")
 
 @app.route("/find_team")
 def find_team():
     query = request.args.get("query")
     if query:
-        results = team.search_teams(query)
+        results = team.search(query)
     else:
         results = []
         query = ""
@@ -47,33 +48,36 @@ def find_team():
 
 @app.route("/team/<int:team_id>")
 def show_team(team_id):
-    team_details = team.get_team_by_id(team_id)
+    team_details = team.get_by_id(team_id)
     return render_template("show_team.html", team=team_details)
 
 @app.route("/edit_team/<int:team_id>", methods=["GET", "POST"])
 def edit_team(team_id):
     if request.method == "GET":
-        team_details = team.get_team_by_id(team_id)
+        team_details = team.get_by_id(team_id)
         return render_template("edit_team.html", team=team_details, message="")
 
     if request.method == "POST":
-        series = request.form["serie_id"]
-        team_name = request.form["team_name"]
-        description = request.form["description"]
+        if "cancel" in request.form:
+            return redirect("/")
+        
+        serie_id = request.form["serie_id"]
+        name = request.form["team_name"].strip()
+        description = request.form["description"].strip()
 
-        try:
-            team.update(team_id, team_name, series, description)
-        except sqlite3.IntegrityError:
+        if not team.is_name_free_in_serie(name, serie_id):
             message = "VIRHE: Joukkueen nimi on jo varattu tässä sarjassa"
-            team_details = team.get_team_by_id(team_id)
-            return render_template("edit_team.html", team=team_details, message=message)
+            team_details = team.get_by_id(team_id)
+            return render_template("edit_team.html", team=team_details,
+                                   message=message)
 
+        team.update(team_id, name, serie_id, description)
         return redirect(f"/team/{team_id}")
     
 @app.route("/remove_team/<int:team_id>", methods=["GET", "POST"])
 def remove_team(team_id):
     if request.method == "GET":
-        team_details = team.get_team_by_id(team_id)
+        team_details = team.get_by_id(team_id)
         return render_template("remove_team.html", team=team_details)
 
     if request.method == "POST":
@@ -88,6 +92,9 @@ def register():
         return render_template("register.html", message="")
     
     if request.method == "POST":
+        if "cancel" in request.form:
+            return redirect("/")
+
         username = request.form["username"]
         password1 = request.form["password1"]
         password2 = request.form["password2"]
@@ -113,6 +120,8 @@ def login():
         return render_template("login.html", message="")
 
     if request.method == "POST":
+        if "cancel" in request.form:
+            return redirect("/")
         username = request.form["username"]
         password = request.form["password"]
 
