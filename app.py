bdb3dfa0bc58732e38ac6ffe15956781ad6da594
Harri R J Lemmelä <harri.lemmela@gmail.com>
2026-02-11 20:15:46 +0200

    Update navigation and naming

--- app.py
+++ app.py
@@ -2,16 +2,16 @@
 from flask import Flask
 from flask import render_template, request, redirect, session
 import config
-import team
-import user
+import teams
+import users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    teams = team.get_all()
-    return render_template("index.html", teams=teams)
+    all_teams = teams.get_all_teams()
+    return render_template("index.html", teams=all_teams)
 
 @app.route("/create_team", methods=["GET", "POST"])
 def new_invite():
@@ -19,26 +19,23 @@ def new_invite():
         return render_template("create_team.html", message="")
     
     if request.method == "POST":
-        if "cancel" in request.form:
-            return redirect("/")
-        
         serie_id = int(request.form["serie_id"])
         team_name = request.form["team_name"].strip()
         description = request.form["description"].strip()
         captain = session["user_id"]
 
-        if not team.is_name_available(team_name, serie_id):
+        if not teams.is_name_available(team_name, serie_id):
             message = "VIRHE: Joukkueen nimi on jo varattu tässä sarjassa"
             return render_template("create_team.html", message=message)
         
-        team.create(team_name, captain, serie_id, description)
+        teams.create_team(team_name, captain, serie_id, description)
         return redirect("/")
 
 @app.route("/find_team")
 def find_team():
     query = request.args.get("query")
     if query:
-        results = team.search(query)
+        results = teams.search(query)
     else:
         results = []
         query = ""
@@ -46,20 +43,20 @@ def find_team():
 
 @app.route("/team/<int:team_id>")
 def show_team(team_id):
-    team_details = team.get_by_id(team_id)
+    team_details = teams.get_team_by_id(team_id)
     return render_template("show_team.html", team=team_details)
 
 @app.route("/edit_team/<int:team_id>", methods=["GET", "POST"])
 def edit_team(team_id):
     if request.method == "GET":
-        team_details = team.get_by_id(team_id)
+        team_details = teams.get_team_by_id(team_id)
         return render_template("edit_team.html", team=team_details, message="")
 
     if request.method == "POST":
         if "cancel" in request.form:
             return redirect("/")
 
-        team_details = team.get_by_id(team_id)   
+        team_details = teams.get_team_by_id(team_id)   
         new_serie_id = int(request.form["serie_id"])
         new_name = request.form["team_name"].strip()
         new_description = request.form["description"].strip()
@@ -67,26 +64,26 @@ def edit_team(team_id):
         name_or_serie_changed = new_name != team_details.name or \
                                 new_serie_id != team_details.serie_id
         if (name_or_serie_changed and not
-            team.is_name_available(new_name, new_serie_id)):
+            teams.is_name_available(new_name, new_serie_id)):
             message = f"VIRHE: Nimi {new_name} on jo käytössä tässä sarjassa"
             team_details.serie_id = new_serie_id
             team_details.description = new_description
             return render_template("edit_team.html", team=team_details,
                                    message=message)
 
-        team.update(team_id, new_name, new_serie_id, new_description)
+        teams.update_team(team_id, new_name, new_serie_id, new_description)
         return redirect(f"/team/{team_id}")
     
 @app.route("/remove_team/<int:team_id>", methods=["GET", "POST"])
 def remove_team(team_id):
     if request.method == "GET":
-        team_details = team.get_by_id(team_id)
+        team_details = teams.get_team_by_id(team_id)
         return render_template("remove_team.html", team=team_details)
 
     if request.method == "POST":
         if "cancel" in request.form:
             return redirect(f"/team/{team_id}")
-        team.remove(team_id)
+        teams.remove_team(team_id)
         return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -95,9 +92,6 @@ def register():
         return render_template("register.html", message="")
     
     if request.method == "POST":
-        if "cancel" in request.form:
-            return redirect("/")
-
         username = request.form["username"].strip()
         password1 = request.form["password1"]
         password2 = request.form["password2"]
@@ -107,7 +101,7 @@ def register():
             message += "Tyhjää tilaa tunnksen alussa ja lopussa ei huomioida."
             return render_template("register.html", message=message)
 
-        if not user.is_username_available(username):
+        if not users.is_username_available(username):
             message = "VIRHE: Tunnus on jo varattu. Yritä uudelleen."
             return render_template("register.html", message=message)
 
@@ -115,7 +109,7 @@ def register():
             message = "VIRHE: salasanat eivät ole samat. Yritä uudelleen."
             return render_template("register.html", message=message)
 
-        user.register(username, password1)          
+        users.register(username, password1)          
         message = "Tunnus luotu"
         return render_template("register.html", message=message)
     
@@ -130,7 +124,7 @@ def login():
         
         username = request.form["username"].strip()
         password = request.form["password"]
-        user_id = user.login(username, password)
+        user_id = users.login(username, password)
 
         if not user_id:
             message = "VIRHE: Virheellinen tunnus tai salasana"
--- teams.py
+++ teams.py
@@ -1,17 +1,17 @@
 import db
 from werkzeug.security import generate_password_hash
 
-def create(name, captain_user_id, serie_id,  description):
+def create_team(name, captain_user_id, serie_id,  description):
     sql = '''INSERT INTO teams (name, user_id, serie_id, description)
              VALUES (?, ?, ?, ?)'''
     db.execute(sql, [name, captain_user_id, serie_id, description])
 
-def update(team_id, name, serie_id, description):
+def update_team(team_id, name, serie_id, description):
     sql = '''UPDATE teams SET name=?, serie_id=?, description=?
              WHERE id=?'''
     db.execute(sql, [name, serie_id, description, team_id])
 
-def remove(team_id):
+def remove_team(team_id):
     name_hash = generate_password_hash(f"{team_id} of team to be removed")
     sql = '''UPDATE teams SET name=?, active=0
              WHERE id=?'''
@@ -28,14 +28,14 @@ def is_name_available(name, serie_id):
     sql = '''SELECT id FROM teams WHERE name = ? AND serie_id = ?'''
     return len(db.query(sql, [name, serie_id])) == 0
 
-def get_all():
+def get_all_teams():
     sql = '''SELECT T.id, T.name, S.description
              FROM teams T LEFT JOIN series S ON T.serie_id = S.id
              WHERE T.active = 1
              ORDER BY T.id DESC'''
     return db.query(sql)
 
-def get_by_id(team_id):
+def get_team_by_id(team_id):
     sql = '''SELECT T.id AS id, T.name AS name, T.description AS description,
              T.serie_id AS serie_id, S.description AS serie_description,
              U.username AS captain, U.id AS captain_id
